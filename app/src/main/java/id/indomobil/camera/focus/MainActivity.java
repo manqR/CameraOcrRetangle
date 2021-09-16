bc710ae3877e603b7cd88693691fced29b828f75
muhamad adinugraha <muhamad.adinugraha@indomobil.local>
2021-09-16 10:55:27 +0700

    rm

--- app/src/main/java/id/indomobil/camera/focus/MainActivity.java
+++ app/src/main/java/id/indomobil/camera/focus/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                 null);
 
         visionBuilder.setVisionRequestInitializer(
-                new VisionRequestInitializer("YOUR-API-GOOGLE-KEY"));
+                new VisionRequestInitializer("AIzaSyBcVa1effgH1NBEGEh8bwpNfh9ZS-mN7-Y"));
 
         vision = visionBuilder.build();
 
